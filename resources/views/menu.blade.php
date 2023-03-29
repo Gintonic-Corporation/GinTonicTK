16bf212b3e0aaa328d2a1b22a15ab18ee123efd4
kulmanferdi <ferdi.kulman@gmail.com>
2023-03-29 17:24:29 +0200

    login beta

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Main menu</title>
+        <link rel="stylesheet" href="resources/css/menu.css" style="stylesheet">
+    </head>
+    <body>
+        <h1>GinTonicTK</h1>   
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,20 @@
 <html>
     <head>
-        <title>Login</title>
+        <title>Login</title>        
+        <link rel="stylesheet" href="resources/css/login.css" style="stylesheet">
     </head>
     <body>
         <h1>GinTonicTK</h1>
         <h2>Bejelentkezés</h2>
+        <form action="/login" method="POST">
+            @csrf
+            <label for="email">Username:</label>
+            <input type="text" id="username" name="username">
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+
+            <button type="submit">Login</button>
+        </form>
     </body>
 </html>
