dffb2febc74c92271e2548c5e0a0cb12c007c912
kulmanferdi <ferdi.kulman@gmail.com>
2023-05-11 19:30:06 +0200

    Icon change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
 		<title>GinTonicTK</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		{{-- <link rel="icon" href="public/favicon.ico" type="image/x-icon"/> --}}
 		{{-- <link rel="stylesheet" href="resources/css/main.css" /> --}}
 		<noscript><link rel="stylesheet" href="resources/css/noscript.css" /></noscript>
         @vite('resources/css/main.css')
